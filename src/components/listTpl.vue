e4e2d049764ef6afa10819cf52e6c3fae1d46403
wohaiwo <zhome621@gmail.com>
2017-05-11 18:29:33 +0800

    :sparkles: :lipstick: add feature about yitiji

--- src/components/listTpl.vue
+++ src/components/listTpl.vue
@@ -1,10 +1,10 @@
 <template>
 	<div>
-		<div class="list-tpl">
-			<ul class="list-item">
+		<div class="list-tpl" >
+			<ul v-if="!isApp" class="list-item">
 				<li v-for="item in items">
-			    	<router-link :to="{name: 'listDetail', params: {id: item.id, identifier: identifier}}">
-						<div class="list-image"  :to="{name: 'listDetail', params: {id: item.id, type: type, identifier: identifier}}">
+			    	<router-link :to="{name: 'listDetail', params: {id: item.id, type: 1, identifier: identifier}}">
+						<div class="list-image">
 							<img :src="item.imageUrl">
 						</div>
 						<aside >
@@ -14,12 +14,32 @@
 	                </router-link>
 				</li>
 			</ul>
+			<ul v-if="isApp" class="list-item">
+				<li v-for="item in items" @click="showQRCode(item.qrCode)">
+					<div class="list-image">
+						<img :src="item.imageUrl">
+					</div>
+					<aside >
+						<h3>{{ item.title }}</h3>
+						<article>{{ item.description }}</article>
+					</aside>
+				</li>
+			</ul>
 	    </div>
+	    <div v-if="isShowQrBox" id="qrcode" @click="closeQrcodeBox"></div>
 	</div>
 </template>
 
 <script>
+	import Vue from 'vue';
+	import '../static/lib/js/jquery.qrcode.min.js';
 	export default {
+		data() {
+			return {
+				isApp: false,
+				isShowQrBox: false
+			}
+		},
 		props: {
 			identifier: {
 				type: String,
@@ -32,8 +52,31 @@
 			type: {
 				type: String
 			}
+		},
+		created() {
+	        if(this.identifier == 1) {
+	        	this.isApp = false;
+	        } else {
+		        let isApp = window.localStorage ? localStorage.getItem('isApp') : Cookie.read('isApp');
+		        if(isApp == 'true') {
+		        	this.isApp = true;
+		        }
+	        }
+		},
+		methods: {
+			showQRCode(url) {
+				this.isShowQrBox = true;
+				jQuery('#qrcode').empty();
+				Vue.nextTick(function() {
+					jQuery('#qrcode').qrcode(url);
+				})
+			},
+			// 关闭二维码框
+			closeQrcodeBox() {
+				this.isShowQrBox = false;
+			}
 		}
-	}		
+	}
 </script>
 <style scoped lang="scss">
 	.list-tpl {
@@ -77,14 +120,27 @@
 						font-weight: bold;
 					}
 					article {
-						height: 12vh;
 						font-size: 14px;
 						color: #000;
-						line-height: 1.5;
+						line-height: 1.4;
 						text-align: justify;
 					}
 				}
 			}
-		}	
+		}
+	}
+	#qrcode {
+		position: absolute;
+		width: 300px;
+		height: 340px;
+		left: 50%;
+		top: 50%;
+		z-index: 100;
+		text-align: center;
+		padding-top: 50px;
+		background: #fff;
+		border: 1px solid #ddd;
+		transform: translate3d(-50%, -50%, 0);
+		box-sizing: border-box;
 	}
 </style>
--- src/page/scenicIntroduction.vue
+++ src/page/scenicIntroduction.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
-        <v-header goBack="true" headTitle="景点介绍"></v-header>
-        <list-tpl :items="scenicInfo"  identifier="1"></list-tpl>
+        <v-header goBack="true" headTitle="景区介绍"></v-header>
+        <list-tpl :items="scenicInfo" :type="type"  identifier="1"></list-tpl>
         <loading :show="done"></loading>
     </div>
 </template>
@@ -15,6 +15,7 @@
       data() {
         return {
             done: false,
+            type: '0',      // 这个类型应该是字符串，需要跟路由匹配到
             scenicInfo: []
         }
       },
@@ -28,16 +29,16 @@
       methods: {
         initPage() {
             this.done = true;
-            let url = '/zhan/querySSSList';
+            let url = '/JSY_H5/h5/querySSSList';
             this.$http.get(url).then((response) => {
                 // 遍历数据，改变数据结构，套用同一套模板listTpl
                 response.data.rows.forEach((item, index) => {
-                  let tmp = {};
-                  tmp['description'] = item['SS_DESCRIPTION'];
-                  tmp['id'] = item['SS_NO'];
-                  tmp['imageUrl'] = item['SS_IMAGE_URL'];
-                  tmp['title'] = item['SS_TITLE'];
-                  this.$data.scenicInfo.push(tmp);
+                    let tmp = {};
+                    tmp['description'] = item['SS_DESCRIPTION'];
+                    tmp['id'] = item['SS_NO'];
+                    tmp['imageUrl'] = item['SS_IMAGE_URL'];
+                    tmp['title'] = item['SS_TITLE'];
+                    this.$data.scenicInfo.push(tmp);
                 });
                 this.$data.done = false;
             }, (response) => {
