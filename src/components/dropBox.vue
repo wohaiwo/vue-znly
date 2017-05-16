94346ea82929843222b3c62cce08b5aff1bf6d3f
wohaiwo <zhome621@gmail.com>
2017-05-16 16:29:58 +0800

    :sparkles: 添加iframe组件模块

---
+++ src/components/dropBox.vue
@@ -0,0 +1,45 @@
+<template>
+    <div>
+	    <v-header goBack="true" :headTitle="title"></v-header>	
+		<iframe :src="url" frameborder="0">
+		</iframe>
+    </div>
+</template>
+
+<script>
+	import vHeader from '../components/header.vue';
+	export default {
+		data() {
+			return {
+				url: '',
+				title: ''
+			}
+		},
+		created() {
+			this.$data.url = this.$route.params.url;
+			this.$data.title = this.$route.params.title;
+		},
+		components: {
+			vHeader
+		}
+	}
+</script>
+
+<style scoped lang="scss">
+	div {
+		width: 100%;
+		height: 100vh;
+		iframe {
+			width: 100%;
+			height: 100%;
+			margin-top: 40px;
+		}
+	}
+	@media screen and (min-width: 1000px) {
+		div {
+			iframe {
+				margin-top: 100px;
+			}
+		}
+	}
+</style>
